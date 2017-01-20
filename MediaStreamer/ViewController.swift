ae1a54c386a915e63ddeaa92b303d5c77a21c3f3
Matt <m.weiss27@yahoo.com>
2017-01-20 14:01:20 -0500

    Initial Commit

---
+++ MediaStreamer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MediaStreamer
+//
+//  Created by Matt Weiss on 1/20/17.
+//  Copyright © 2017 Matt Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
