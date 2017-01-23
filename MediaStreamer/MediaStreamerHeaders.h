532cac4c6bf4dae5c2c22a559e059f7ecf9a4f3a
Matt <m.weiss27@yahoo.com>
2017-01-23 17:31:34 -0500

    Tinkering with frameworks

---
+++ MediaStreamer/MediaStreamerHeaders.h
@@ -0,0 +1,17 @@
+//
+//  MediaStreamer.h
+//  MediaStreamer
+//
+//  Created by Matt Weiss on 1/23/17.
+//  Copyright © 2017 Matt Weiss. All rights reserved.
+//
+
+#ifndef MediaStreamer_h
+#define MediaStreamer_h
+
+
+#endif /* MediaStreamer_h */
+
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SafariServices/SafariServices.h>
