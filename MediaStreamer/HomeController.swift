f74d5e8c75f105a6126786c579921217162e3e88
Adam Tyler <adamtyler07@gmail.com>
2017-02-09 20:25:26 -0500

    Start on Socket work client side

--- MediaStreamer/HomeController.swift
+++ MediaStreamer/HomeController.swift
@@ -7,20 +7,43 @@
 //
 
 import UIKit
+import SocketIO
 
 class HomeController: UIViewController {
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
+        let uuid = UUID().uuidString
+        print(uuid)
+        self.initNetworkCommunication()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         print("HomeController is displayed")
+    }
+    
+    func initNetworkCommunication(){
+        let socket = SocketIOClient(socketURL: URL(string: "http://192.168.1.117:80")!, config: [.log(true), .forcePolling(true)])
+        
+        socket.on("connect") {data, ack in
+            print("socket connected")
+        }
+        
+        socket.on("currentAmount") {data, ack in
+            if let cur = data[0] as? Double {
+                socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
+                    socket.emit("update", ["amount": cur + 2.50])
+                }
+                
+                ack.with("Got your currentAmount", "dude")
+            }
+        }
+        
+        socket.connect()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
