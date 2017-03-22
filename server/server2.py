b6c268979ccab01d8561a48e62474f6b3c6a8af1
Matt <m.weiss27@yahoo.com>
2017-03-22 19:08:16 -0400

    .

--- server/server2.py
+++ server/server2.py
@@ -424,7 +424,7 @@ def getQueue():
 		i = 0
 		for item in Queue.queue:
 			result.append([str(Room.playing and i == 0), str(item.id), str(item.name), str(Room.response_time), str(Room.playback_time)])
-			i++
+			i+=1
 		return jsonify(queue=result)
 	else:
 		msg = "[ERROR] roomNum not in room2queue: " + str(roomNum) + " -- " + str(room2queue)
